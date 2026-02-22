3fc02bc37237eaf86b8d0254d2d7a162739a2cff
Dauntess-Alexandro <alexgrs2097@gmail.com>
2026-02-22 15:12:50 +0300

    Fix AlphaZero env creation with roster-based Warhammer init

--- train_alphazero.py
+++ train_alphazero.py
@@ -5,13 +5,14 @@ import datetime
 import json
 import os
 import pickle
-import random
-import time
 from dataclasses import dataclass
 
 import matplotlib.pyplot as plt
 import torch
 import torch.nn.functional as F
+
+from gym_mod.engine import Unit, initFile, unitData, weaponData
+from gym_mod.engine.mission import board_dims_for_mission, normalize_mission_name
 
 from model.alphazero import (
     AlphaZeroMCTS,
@@ -26,6 +27,129 @@ from model.alphazero import (
     safe_env_copy,
     to_action_dict,
 )
+
+
+DEFAULT_MISSION_NAME = "only_war"
+
+
+def _load_roster_config():
+    config = {
+        "totLifeT": 10,
+        "b_len": 40,
+        "b_hei": 60,
+        "mission": DEFAULT_MISSION_NAME,
+        "enemy_faction": "Necrons",
+        "model_faction": "Necrons",
+        "enemy_units": [
+            {
+                "name": "Necron Warriors",
+                "weapons": ("Gauss flayer", "Necron close combat weapon"),
+                "count": None,
+                "instance_id": None,
+            },
+            {
+                "name": "Royal Warden",
+                "weapons": ("Relic gauss blaster", "Royal warden close combat weapon"),
+                "count": None,
+                "instance_id": None,
+            },
+        ],
+        "model_units": [
+            {
+                "name": "Necron Warriors",
+                "weapons": ("Gauss flayer", "Necron close combat weapon"),
+                "count": None,
+                "instance_id": None,
+            },
+            {
+                "name": "Royal Warden",
+                "weapons": ("Relic gauss blaster", "Royal warden close combat weapon"),
+                "count": None,
+                "instance_id": None,
+            },
+        ],
+    }
+
+    if os.path.isfile("gui/data.json"):
+        config["totLifeT"] = initFile.getNumLife()
+        config["b_len"] = initFile.getBoardX()
+        config["b_hei"] = initFile.getBoardY()
+        config["enemy_faction"] = initFile.getEnemyFaction()
+        config["model_faction"] = initFile.getModelFaction()
+        config["mission"] = normalize_mission_name(getattr(initFile, "getMission", lambda: DEFAULT_MISSION_NAME)())
+
+        enemy_counts = initFile.getEnemyUnitCounts()
+        model_counts = initFile.getModelUnitCounts()
+        enemy_instance_ids = initFile.getEnemyUnitInstanceIds()
+        model_instance_ids = initFile.getModelUnitInstanceIds()
+
+        enemy_units = []
+        for i in range(len(initFile.getEnemyUnits())):
+            count = enemy_counts[i] if i < len(enemy_counts) and enemy_counts[i] > 0 else None
+            instance_id = enemy_instance_ids[i] if i < len(enemy_instance_ids) else None
+            enemy_units.append(
+                {
+                    "name": initFile.getEnemyUnits()[i],
+                    "weapons": (initFile.getEnemyW()[i][0], initFile.getEnemyW()[i][1]),
+                    "count": count,
+                    "instance_id": instance_id,
+                }
+            )
+
+        model_units = []
+        for i in range(len(initFile.getModelUnits())):
+            count = model_counts[i] if i < len(model_counts) and model_counts[i] > 0 else None
+            instance_id = model_instance_ids[i] if i < len(model_instance_ids) else None
+            model_units.append(
+                {
+                    "name": initFile.getModelUnits()[i],
+                    "weapons": (initFile.getModelW()[i][0], initFile.getModelW()[i][1]),
+                    "count": count,
+                    "instance_id": instance_id,
+                }
+            )
+
+        config["enemy_units"] = enemy_units
+        config["model_units"] = model_units
+
+    config["mission"] = normalize_mission_name(config.get("mission", os.getenv("MISSION_NAME", DEFAULT_MISSION_NAME)))
+    config["b_len"], config["b_hei"] = board_dims_for_mission(config["mission"])
+    return config
+
+
+def _build_units_from_config(config, b_len, b_hei):
+    enemy = []
+    model = []
+    for spec in config["enemy_units"]:
+        unit_data = unitData(config["enemy_faction"], spec["name"])
+        if spec["count"]:
+            unit_data["#OfModels"] = spec["count"]
+        enemy.append(
+            Unit(
+                unit_data,
+                weaponData(spec["weapons"][0]),
+                weaponData(spec["weapons"][1]),
+                b_len,
+                b_hei,
+                instance_id=spec["instance_id"],
+            )
+        )
+
+    for spec in config["model_units"]:
+        unit_data = unitData(config["model_faction"], spec["name"])
+        if spec["count"]:
+            unit_data["#OfModels"] = spec["count"]
+        model.append(
+            Unit(
+                unit_data,
+                weaponData(spec["weapons"][0]),
+                weaponData(spec["weapons"][1]),
+                b_len,
+                b_hei,
+                instance_id=spec["instance_id"],
+            )
+        )
+    return enemy, model
 
 
 @dataclass
@@ -98,7 +222,18 @@ def _env_factory():
     import gymnasium as gym
     import gym_mod  # noqa: F401
 
-    return gym.make("40kAI-v0")
+    roster = _load_roster_config()
+    enemy, model = _build_units_from_config(roster, roster["b_len"], roster["b_hei"])
+    env = gym.make(
+        "40kAI-v0",
+        disable_env_checker=True,
+        enemy=enemy,
+        model=model,
+        b_len=roster["b_len"],
+        b_hei=roster["b_hei"],
+    )
+    env.reset(options={"m": model, "e": enemy, "Type": "small", "trunc": True})
+    return env
 
 
 def main() -> int:
